704b4349c731ae57627bbcd0ad8c02754bd6abf9
Dave Preece <dpreece@iweb.co.uk>
2014-08-04 21:36:06 +0100

    Remove header notice, custom profile page

--- functions.php
+++ functions.php
@@ -3,6 +3,12 @@
 add_action('wp', 'avada_child_setup');
 add_filter('avada_after_header', 'header_messages');
 
+add_action('tml_template', 'adapt_tml_filter_paths', 10, 3);
+add_filter('hide-header-notice', 'check_hide_header_notice');
+
+// Remove the styling of the login page which theme-my-login adds by default
+remove_filter('site_url', array(Theme_My_Login::get_object(), 'site_url'), 10);
+
 function avada_child_setup(){
 	wp_deregister_script('jquery');
 	wp_enqueue_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');
@@ -14,7 +20,25 @@ function avada_child_setup(){
 
 	add_filter('wp_nav_menu_items','extra_nav_menu_items', 10, 2);
 	add_action("login_head", "login_head");
+   
 }
+
+function check_hide_header_notice(){
+    $parts = parse_url($_SERVER['REQUEST_URI']);
+    $page = trim($parts['path'], '/');
+
+    if($page == 'your-profile'){
+        return true;
+    }
+}
+
+// Use the profile form in our theme rather than this one (in TML)
+function adapt_tml_filter_paths($path, $locations){
+    if(in_array('profile-form.php', $locations)){
+        return get_stylesheet_directory() . DIRECTORY_SEPARATOR . "template" . DIRECTORY_SEPARATOR . "profile-form.php";
+    }
+}
+
 function extra_nav_menu_items( $items, $args ) {
 
 	if(isset($args->menu) && $args->menu == '' && is_user_logged_in()){
